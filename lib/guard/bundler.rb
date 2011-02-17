cbe806bf258c5871c3e82b45d6048a42827c4aa2
hoverlover <hoverlover@gmail.com>
2011-02-17 10:27:12 -0600

    Now working with `bundle exec`.

--- lib/guard/bundler.rb
+++ lib/guard/bundler.rb
@@ -4,12 +4,14 @@ require 'guard/guard'
 
 module Guard
   class Bundler < Guard
+    BUNDLER_ENV_VARS = %w(RUBYOPT BUNDLE_PATH BUNDLE_BIN_PATH BUNDLE_GEMFILE).freeze
 
     autoload :Notifier, 'guard/bundler/notifier'
 
     def initialize(watchers = [], options = {})
       super
 
+      @original_env = {}
       options[:notify] = true if options[:notify].nil?
     end
  
@@ -38,14 +40,38 @@ module Guard
     end
 
     def bundle_need_refresh?
-      `bundle check`
+      with_clean_env do
+        `bundle check`
+      end
       $? == 0 ? false : true
+    end
+
+    def with_clean_env
+      unset_bundler_env_vars
+      ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, "Gemfile")
+      yield
+    ensure
+      restore_env
+    end
+
+    def unset_bundler_env_vars
+      BUNDLER_ENV_VARS.each do |key|
+        @original_env[key] = ENV[key]
+        ENV[key] = nil
+      end
+    end
+
+    def restore_env
+      @original_env.each { |key, value| ENV[key] = value }
     end
 
     def refresh_bundle
       UI.info 'Refresh bundle', :reset => true
       start_at = Time.now
-      result = system('bundle install')
+      result = ''
+      with_clean_env do
+        result = system('bundle install')
+      end
       Notifier::notify(true, Time.now - start_at) if notify?
       result
     end
