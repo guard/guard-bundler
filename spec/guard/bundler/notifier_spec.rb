7bbf615f30f78ae382cd70f08e3a0ff2981b3dbc
Cezary Baginski <cezary@chronomantic.net>
2014-12-06 17:22:27 +0100

    Convert specs to RSpec 3.1.7 syntax with Transpec
    This conversion is done by Transpec 2.3.8 with the following command:
        transpec
    
    * 17 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 8 conversions
        from: obj.should
          to: expect(obj).to
    
    * 4 conversions
        from: == expected
          to: eq(expected)
    
    * 3 conversions
        from: be_true
          to: be_truthy
    
    * 3 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 2 conversions
        from: obj.should_not_receive(:message)
          to: expect(obj).not_to receive(:message)
    
    * 1 conversion
        from: be_false
          to: be_falsey
    
    * 1 conversion
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/guard/bundler/notifier_spec.rb
+++ spec/guard/bundler/notifier_spec.rb
@@ -5,24 +5,24 @@ RSpec.describe Guard::Bundler::Notifier do
 
   it 'should format success message' do
     message = subject.guard_message(true, 10.1)
-    message.should == "Bundle has been installed\nin 10.1 seconds."
+    expect(message).to eq("Bundle has been installed\nin 10.1 seconds.")
   end
 
   it 'should format fail message' do
     message = subject.guard_message(false, 10.1)
-    message.should == "Bundle can't be installed,\nplease check manually."
+    expect(message).to eq("Bundle can't be installed,\nplease check manually.")
   end
 
   it 'should select success image' do
-    subject.guard_image(true).should == :success
+    expect(subject.guard_image(true)).to eq(:success)
   end
 
   it 'should select failed image' do
-    subject.guard_image(false).should == :failed
+    expect(subject.guard_image(false)).to eq(:failed)
   end
 
   it 'should call Guard::Notifier' do
-    ::Guard::Notifier.should_receive(:notify).with(
+    expect(::Guard::Notifier).to receive(:notify).with(
       "Bundle has been installed\nin 10.1 seconds.",
       title: 'bundle install',
       image: :success
--- spec/guard/bundler_spec.rb
+++ spec/guard/bundler_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe Guard::Bundler do
   context '#bundle_check' do
 
     it 'should call `bundle check\' command' do
-      subject.should_receive(:'`').with('bundle check')
+      expect(subject).to receive(:'`').with('bundle check')
       subject.send(:bundle_check)
     end
 
     context 'arbitrary cli switches' do
 
       it 'should be false by default' do
-        subject.should_not be_cli
+        expect(subject).not_to be_cli
       end
 
       it 'should be set to true' do
         subject = Guard::Bundler.new(cli: '--binstubs')
-        subject.options[:cli].should be_true
+        expect(subject.options[:cli]).to be_truthy
       end
 
     end
@@ -30,7 +30,7 @@ RSpec.describe Guard::Bundler do
   context '#bundle_install' do
 
     it 'should call `bundle install\' command' do
-      subject.should_receive(:system).with('bundle install')
+      expect(subject).to receive(:system).with('bundle install')
       subject.send(:bundle_install)
     end
 
@@ -39,40 +39,40 @@ RSpec.describe Guard::Bundler do
   context '#refresh_bundle' do
 
     it 'should call #bundle_install if #bundle_check fails' do
-      subject.should_receive(:bundle_check).and_return(false)
-      subject.should_receive(:bundle_install).and_return(:bundle_installed)
-      subject.send(:refresh_bundle).should be_true
+      expect(subject).to receive(:bundle_check).and_return(false)
+      expect(subject).to receive(:bundle_install).and_return(:bundle_installed)
+      expect(subject.send(:refresh_bundle)).to be_truthy
     end
 
     it 'should not call #bundle_install if #bundle_check succeeds' do
-      subject.should_receive(:bundle_check).and_return(:bundle_already_up_to_date)
-      subject.should_not_receive(:bundle_install)
-      subject.send(:refresh_bundle).should be_true
+      expect(subject).to receive(:bundle_check).and_return(:bundle_already_up_to_date)
+      expect(subject).not_to receive(:bundle_install)
+      expect(subject.send(:refresh_bundle)).to be_truthy
     end
 
     it 'should return false if #bundle_install fails' do
-      subject.should_receive(:bundle_check).and_return(false)
-      subject.should_receive(:bundle_install).and_return(false)
-      subject.send(:refresh_bundle).should be_false
+      expect(subject).to receive(:bundle_check).and_return(false)
+      expect(subject).to receive(:bundle_install).and_return(false)
+      expect(subject.send(:refresh_bundle)).to be_falsey
     end
 
     it 'should call notifier after #bundle_install succeeds' do
-      subject.stub(:bundle_check).and_return(false)
-      subject.should_receive(:bundle_install).and_return(:bundle_installed)
-      Guard::Bundler::Notifier.should_receive(:notify).with(true, anything())
+      allow(subject).to receive(:bundle_check).and_return(false)
+      expect(subject).to receive(:bundle_install).and_return(:bundle_installed)
+      expect(Guard::Bundler::Notifier).to receive(:notify).with(true, anything())
       subject.send(:refresh_bundle)
     end
 
     it 'should call notifier after #bundle_install fails' do
-      subject.stub(:bundle_check).and_return(false)
-      subject.should_receive(:bundle_install).and_return(false)
-      Guard::Bundler::Notifier.should_receive(:notify).with(false, anything())
+      allow(subject).to receive(:bundle_check).and_return(false)
+      expect(subject).to receive(:bundle_install).and_return(false)
+      expect(Guard::Bundler::Notifier).to receive(:notify).with(false, anything())
       subject.send(:refresh_bundle)
     end
 
     it 'should call notifier if #bundle_check succeeds' do
-      subject.stub(:bundle_check).and_return(:bundle_installed_using_local_gems)
-      Guard::Bundler::Notifier.should_not_receive(:notify).with('up-to-date', anything())
+      allow(subject).to receive(:bundle_check).and_return(:bundle_installed_using_local_gems)
+      expect(Guard::Bundler::Notifier).not_to receive(:notify).with('up-to-date', anything())
       subject.send(:refresh_bundle)
     end
 
@@ -81,7 +81,7 @@ RSpec.describe Guard::Bundler do
   context '#start' do
 
     it 'should call #refresh_bundle' do
-      subject.should_receive(:refresh_bundle).and_return(true)
+      expect(subject).to receive(:refresh_bundle).and_return(true)
       subject.start
     end
 
@@ -90,7 +90,7 @@ RSpec.describe Guard::Bundler do
   context '#reload' do
 
     it 'should call #refresh_bundle' do
-      subject.should_receive(:refresh_bundle).and_return(true)
+      expect(subject).to receive(:refresh_bundle).and_return(true)
       subject.reload
     end
 
@@ -99,7 +99,7 @@ RSpec.describe Guard::Bundler do
   context '#run_all' do
 
     it 'should call #refresh_bundle' do
-      subject.should_receive(:refresh_bundle).and_return(true)
+      expect(subject).to receive(:refresh_bundle).and_return(true)
       subject.run_all
     end
 
@@ -108,7 +108,7 @@ RSpec.describe Guard::Bundler do
   context '#run_on_additions' do
 
     it 'should call #refresh_bundle' do
-      subject.should_receive(:refresh_bundle).and_return(true)
+      expect(subject).to receive(:refresh_bundle).and_return(true)
       subject.run_on_additions
     end
 
@@ -117,7 +117,7 @@ RSpec.describe Guard::Bundler do
   context '#run_on_modifications' do
 
     it 'should call #refresh_bundle' do
-      subject.should_receive(:refresh_bundle).and_return(true)
+      expect(subject).to receive(:refresh_bundle).and_return(true)
       subject.run_on_modifications
     end
 
